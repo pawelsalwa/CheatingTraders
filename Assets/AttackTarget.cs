48e61dd613911fb8bbf78d9977dd810d3124cb6a
siewcasalwus <siewcasalwus@gmail.com>
2019-08-30 23:22:35 +0200

    *: chamsko na asyncu textura na czerwono na TakeDamage

--- Assets/AttackTarget.cs
+++ Assets/AttackTarget.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Threading.Tasks;
 
 ///<summary> Component being able to take damage </summary>
 [RequireComponent(typeof(Collider), typeof(Rigidbody))]
@@ -11,8 +12,20 @@ public class AttackTarget : MonoBehaviour {
 
     public Material damageTakingMaterial;
 
+    public SkinnedMeshRenderer meshRenderer;
+
+    [Range(0 , 500)]
+    public int materialchangeMiliseconds = 80;
+
     public void TakeDamage(int damage) {
         Debug.Log("damage taken " + damage, gameObject);
+        AnimateMaterialColorAsync();
         OnDamageTaken(damage);
+    }
+
+    private async void AnimateMaterialColorAsync() {
+        meshRenderer.material.color = Color.red;
+        await Task.Delay(materialchangeMiliseconds);
+        meshRenderer.material.color = Color.white;
     }
 }
