33d599d861a24bb7e3d108c975f51cd5827ec8ce
siewcasalwus <siewcasalwus@gmail.com>
2019-08-31 14:26:37 +0200

    *: basic network

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;  
+using Cinemachine;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    public Transform cameraOrbitCenter;
+    public Transform cameraFollowTransform;
+
+    private UserInputHandler _userInputHandler;
+	private UserInputHandler userInputHandler => _userInputHandler == null ? _userInputHandler = GetComponent<UserInputHandler>() : _userInputHandler;
+
+    void Start() {
+        if (isLocalPlayer) {
+            userInputHandler.enabled = true;
+            GM.instance.cinemachineFreeLook.m_Follow = cameraFollowTransform;
+            GM.instance.cinemachineFreeLook.m_LookAt = cameraOrbitCenter;
+        }
+    }
+}
